149d6963cd8022521d7644bee40970231aebbfca
Oliver Woodings <oliver.woodings@gmail.com>
2020-12-17 17:35:37 +0000

    Whoops

--- app/index.js
+++ app/index.js
@@ -4,10 +4,10 @@ const log = require('./lib/log')
 const sentry = require('./lib/sentry')
 const syncFileview = require('./syncFileview')
 
+let locked = false
+
 log.info(`Running on schedule: ${config.schedule}`)
 new CronJob(config.schedule, run, null, true, null, null, config.runOnStart)
-
-let locked = false
 
 async function run () {
   if (locked) {
